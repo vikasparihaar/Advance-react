1b70673cfd34677a7d187844e24c31e48af56531
KUMAR Vikas <vikasparihar864@gmail.com>
2021-08-11 10:27:02 +0530

    remove items

--- src/tutorial/UseStateArray.js
+++ src/tutorial/UseStateArray.js
@@ -1,23 +1,28 @@
 import React from 'react'
-import {data} from "./data";
+import { data } from "./data";
 function UseStateArray() {
-    const[people,setPeople]=React.useState(data)
+    const [people, setPeople] = React.useState(data)
+    const removeItem = (id) => {
+        let newPeople = people.filter((person) => person.id !== id);
+        setPeople(newPeople);
+    }
     return (
-      <>
-      {
-          people.map((person)=>{
-              const{id,name}=person;
-              return(
-                  <div key={id} className='item'>
-                      <h4>{name}</h4>
-                  </div>
-              )
-          })
-      }
-      <button className="btn" onClick={() =>setPeople([])}>
-          Clear the cartoons
-      </button>
-      </>
-)
+        <>
+            {
+                people.map((person) => {
+                    const { id, name } = person;
+                    return (
+                        <div key={id} className='item'>
+                            <h4>{name}</h4>
+                            <button onClick={() => removeItem(id)}>remove item</button>
+                        </div>
+                    )
+                })
+            }
+            <button className="btn" onClick={() => setPeople([])}>
+                Clear the cartoons
+            </button>
+        </>
+    )
 }
 export default UseStateArray
